d956de3ee5f32885b8ec3dc9ed15f1ca44a3cdb6
Mathis Langella <matislangella@gmail.com>
2025-10-07 08:05:29 +0100

    debut d un jeu

--- src/serveur/server.go
+++ src/serveur/server.go
@@ -2,16 +2,38 @@ package serveur
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-// REFERENCE: https://blog.logrocket.com/creating-a-web-server-with-golang
-func Serveur() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	execDir, _ := os.Getwd()
+	tmplPath := filepath.Join(execDir, "templates", "index.html")
 
-	fmt.Printf("Starting server at port 8080\n")
+	if _, err := os.Stat(tmplPath); os.IsNotExist(err) {
+		http.Error(w, "Fichier introuvable : "+tmplPath, http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Erreur de chargement du template : "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func Serveur() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", indexHandler)
+
+	fmt.Println("Serveur démarré sur http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
