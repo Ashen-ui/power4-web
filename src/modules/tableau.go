10404c72f37f7595e3c5819504873177aca44106
Anme Kazaki <deaduntil730@gmail.com>
2025-10-07 09:31:01 +0200

    merge push 1

--- src/modules/tableau.go
+++ src/modules/tableau.go
@@ -1,5 +1,7 @@
 package module
 
+import "fmt"
+
 func InitPlateau() [6][7]string {
 	var plateau [6][7]string = [6][7]string{
 		[7]string{"| - |", "| - |", "| - |", "| - |", "| - |", "| - |", "| - |"},
@@ -15,4 +17,12 @@ func InitPlateau() [6][7]string {
 // color = "| X |" ou "| O |"
 func AddToPlateau(X int, Y int, color string, plateau *[6][7]string) {
 	plateau[X][Y] = color
+}
+
+func PrintPlateau(plateau *[6][7]string) {
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 7; j++ {
+			fmt.Print(plateau[i][j])
+		}
+	}
 }
